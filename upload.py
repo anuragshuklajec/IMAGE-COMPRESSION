57cb708350c69fc66aaac6a7a0a48bc5fde3bf42
Anurag Shukla <76862299+anuragshuklajec@users.noreply.github.com>
2023-05-27 12:49:59 +0530

    Update upload.py
    Hid Authorization Code

--- upload.py
+++ upload.py
@@ -7,14 +7,14 @@ class UploadFileToDrive():
         pass
     def uploadFile(self):
         headers = {
-            "Authorization":"Bearer ya29.a0Ael9sCO7JVaoM2MMe_W4DLHOOydYGf6mD_wRxWxIuAoITDRtRShbfNJe48QPX8NejS1d71LS7JHkyYSIFoAuO8tYsv1V-2iZbWHJY3ed3c0Pn5YhJt6T6MhWONtrOcgL3uxwZsAxv6wVKGdyxvUrw3J1R9VPaCgYKATsSARASFQF4udJhWyPnPeqWg1Vq26TeqBgOyA0163"
+            "Authorization":"{Drive Authorization Code}"
             }
 
 
 
         para = {
             "name":'compressed.json',
-            "parents" : ["1KBozckctynWufOLTiJSOaTvoUFvcpm_a"]
+            "parents" : ["{Folder Key}"]
             }
 
         files = {
